206560c596ec63cd813e71ef0fb6cd6df580e80d
Patrick Kennedy <pkennedytx1@gmail.com>
2022-06-03 17:04:10 -0500

    updated the styles and can now use flex in various ways to make our page look better, also added search on enter key

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ function getBreweryDataByName(searchQuery) {
     fetch(`https://api.openbrewerydb.org/breweries/search?query=${searchQuery}`)
         .then(response => response.json())
         .then((json) => {
+            document.getElementById('results').innerHTML = "";
             json.forEach((brewery, i) => {
                 const breweryText = {
                     name: brewery.name,
@@ -19,6 +20,7 @@ function getBreweryDataByCity(searchQuery) {
     fetch(`https://api.openbrewerydb.org/breweries?by_city=${searchQuery}`)
         .then(response => response.json())
         .then((json) => {
+            document.getElementById('results').innerHTML = "";
             json.forEach((brewery, i) => {
                 const breweryText = {
                     name: brewery.name,
@@ -30,6 +32,16 @@ function getBreweryDataByCity(searchQuery) {
         });
 }
 
+// Allows us to use the enter key to search a param
+const input = document.getElementById('brewery-serach-text');
+
+input.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('search-brewery-button').click();
+    };
+});
+
 function getBreweryData() {
     const searchQuery = document.getElementById('brewery-serach-text').value;
     if (!searchQuery) {
@@ -39,7 +51,7 @@ function getBreweryData() {
         document.getElementById('error-message').innerHTML = "";
     }
     const searchParam = document.querySelector('input[name="search-type"]:checked').value;
-    document.getElementById('results').innerHTML = "";
+    document.getElementById('results').innerHTML = `<div class="d-flex justify-content-center"><div class="spinner-border text-danger" role="status"><span class="visually-hidden">Loading...</span></div></div>`;
     if (searchParam === "name") {
         getBreweryDataByName(searchQuery);
     } else {
@@ -49,7 +61,8 @@ function getBreweryData() {
 
 function displayResults(breweryText) {
     const resultsContainer = document.createElement("div");
-    resultsContainer.innerHTML = `<div class="card" style="width: 18rem;">
+    resultsContainer.setAttribute('class', 'col-sm-12 col-md-6 col-lg-4')
+    resultsContainer.innerHTML = `<div class="card" style="width: auto; min-height: 150px; display: flex; margin-bottom: 18px;">
     <div class="card-body">
       <h5 class="card-title">${breweryText.name}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${breweryText.state}</h6>
@@ -58,10 +71,6 @@ function displayResults(breweryText) {
   </div>`
   document.getElementById('results').appendChild(resultsContainer);
 }
-
-// Todo: Need to make the getBreweryData function execute on search as well as take in a query and apply it to the request.
-
-// Todo: Add another fetch to grab brewerydataby city
 
 // Todo: Display only the frist 10 results in the DOM with basic info.
 
